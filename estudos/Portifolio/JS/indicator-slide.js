b015a4d13d348752de6297a0304133e0473176b6
Mauricio JR <mjunior.dev@outlook.com>
2023-05-01 18:37:00 -0300

    btn scroll top

--- estudos/Portifolio/JS/indicator-slide.js
+++ estudos/Portifolio/JS/indicator-slide.js
@@ -3,14 +3,10 @@ const maxHeight = document.body.scrollHeight - window.innerHeight;
 
 window.addEventListener('scroll', () => {
    const percentage = (window.scrollY / maxHeight) * 100;
-
    indicator.style.width = `${percentage}%`;
 });
 
-// document.querySelector("#itens").addEventListener("wheel", event => {
-//    if (event.deltaY > 0) {
-//       event.target.scrollBy(600, 0)
-//    } else {
-//       event.target.scrollBy(-600, 0)
-//    }
-// })
+const stop = document.querySelector('.Stop');
+stop.addEventListener('click', () => {
+   window.scrollTo(0, 0);
+});
