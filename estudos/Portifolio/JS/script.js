be2774d1fcf11a1a5fc29f206877332b613c72fd
Mauricio JR <mjunior.dev@outlook.com>
2023-05-10 17:58:29 -0300

    Ilustração digital grid

--- estudos/Portifolio/JS/script.js
+++ estudos/Portifolio/JS/script.js
@@ -1,9 +1,9 @@
 
-ScrollReveal().reveal(".Artista",
-      {duration: 1000, reset: true})
+// ScrollReveal().reveal(".Artista",
+//       {duration: 1000, reset: true})
 
-ScrollReveal().reveal(".Ilustração-Digital",
-      { duration: 2000, reset: true})
+// ScrollReveal().reveal(".Ilustração-Digital",
+//       { duration: 2000, reset: true})
 
-ScrollReveal().reveal(".image",
-      {inteval: 2000, delay: 200, reset: true, scale: 1.1})
+// ScrollReveal().reveal(".image",
+//       {inteval: 2000, delay: 200, reset: true, scale: 1.1})
