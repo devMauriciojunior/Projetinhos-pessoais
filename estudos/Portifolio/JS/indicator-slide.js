273c9f4f8c60cd87ee197d813cec86ae3669225c
Mauricio JR <mjunior.dev@outlook.com>
2023-05-15 18:31:50 -0300

    menu hamburguer & media query

--- estudos/Portifolio/JS/indicator-slide.js
+++ estudos/Portifolio/JS/indicator-slide.js
@@ -10,3 +10,16 @@ const stop = document.querySelector('.Stop');
 stop.addEventListener('click', () => {
    window.scrollTo(0, 0);
 });
+
+const btn = document.querySelector('.menu')
+const btnIcon = document.querySelector('.menu i')
+const menuSus = document.querySelector('.menu-suspenso')
+
+   btn.onclick = function () {
+      menuSus.classList.toggle('aberto')
+      const estaAberto = menuSus.classList.contains('aberto')
+
+      btnIcon.classList = estaAberto
+      ? 'fa-solid fa-xmark'
+      : 'fa-solid fa-bars'
+      }
